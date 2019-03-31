79f0be2eff29ea70656f37cb1ddf60e0620df731
axiba <893364516@qq.com>
2019-03-31 10:48:39 +0800

    添加testGit工程到git

---
+++ idea-git-test/src/main/java/com/git/test/testGit.java
@@ -0,0 +1,9 @@
+package com.git.test;
+
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("testGit");
+        System.out.println("testGit");
+        System.out.println("testGit");
+    }
+}
